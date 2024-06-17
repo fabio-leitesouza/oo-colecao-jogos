75c1ae30da48d86d4a8c5577a3fb29bcfd1188a9
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-06-17 08:28:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from modelos.jogo import Jogo
 jogo_zelda = Jogo('Zelda', 'RPG')
 jogo_megaman = Jogo('MegaMan', 'plataforma')
 jogo_metroid = Jogo('Metroid', 'plataforma')
-jogo_sonic = Jogo('soni', 'plataforma')
+jogo_sonic = Jogo('sonic', 'plataforma')
 
 jogo_metroid.alterar_status()
 
-jogo_metroid.recebe_nota('Souza', 10)
-jogo_metroid.recebe_nota('Eduarda', 8)
-jogo_metroid.recebe_nota('Pedro', 7)
+# jogo_metroid.recebe_nota('Souza', 10)
+# jogo_metroid.recebe_nota('Eduarda', 8)
+# jogo_metroid.recebe_nota('Pedro', 7)
 
 def main():
     Jogo.listar_jogos()
